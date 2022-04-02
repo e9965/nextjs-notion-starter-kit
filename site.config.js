5e6afbe6763112538ed4f760318b1c758f436194
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2022-04-02 22:30:11 +0800

    Update

--- site.config.js
+++ site.config.js
@@ -4,6 +4,7 @@ export default {
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
+  //new
   rootNotionSpaceId: null,
 
   // basic site info (required)
